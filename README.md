51125671bc1726ca05a60328e064a5a0eb33559d
yfsoftcom <yfsoftcom@126.com>
2017-03-02 11:41:01 +0800

    update yf-fpm-server

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # FPM-SERVER STARTER
 
 fpm-server 的默认初始项目，可运行基本的fpm-server服务
+
+## Install
+
+```bash
+$ git clone https://github.com/team4yf/yf-fpm-server-starter.git
+```
+
+## Npm Install
+
+```bash
+$ cd yf-fpm-server-starter && npm(cnpm) i
+```
+
+## Run
+
+```
+$ npm run server
+```
+
+OR
+
+```
+$ node app.babel.js
+```
+
+
--- app.js
+++ app.js
@@ -1,37 +1,12 @@
 import { Fpm, Hook,Biz }  from 'yf-fpm-server';
-//加载配置信息的中间件，默认通过编码中的代码信息加载
-import configuration from './config.js';
-
-global.__env = 'DEV';
-let fpm = new Fpm();
-
-fpm.use(configuration);
-//*
+let app = new Fpm();
 let biz = new Biz('0.0.1');
-
 biz.addSubModules('test',{
-	foo13:function(args){
-		console.log('output from app.js');
-		console.log(args);
+	foo:async function(args){
 		return new Promise( (resolve, reject) => {
 			reject({errno: -3001});
-		})
+		});
 	}
 })
-
-fpm.addBizModules(biz);
-
-let hook = new Hook();
-hook.addBeforeHook('test.foo13', (args) => {
-	return new Promise( (resolve, reject) => {
-		console.log('run before hook from app.js');
-		console.log(args);
-		resolve('ok');
-	});
-
-},['0.0.1','0.0.2']);
-
-// fpm.addHook(hook);
-//*/
-const httpPort = 9999;
-fpm.run(httpPort);
+app.addBizModules(biz);
+app.run();
